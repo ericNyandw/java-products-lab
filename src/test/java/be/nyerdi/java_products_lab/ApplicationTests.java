989bc9ded86f2790730b9dc02b19dc7fccfbd50a
nyandwi <eric.nyandwi.didier@gmail.com>
2026-03-17 15:27:24 +0100

    first commit

---
+++ src/test/java/be/nyerdi/java_products_lab/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.nyerdi.java_products_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
